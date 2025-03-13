782f1f85adf6b0d5dab8de225b117bbc33cccd12
Scott12565 <nkosinathinkosi153@gmail.com>
2025-03-13 08:39:54 +0200

    updated the server.js to serve static files

--- server.js
+++ server.js
@@ -1,49 +1,54 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require("path");  // Add this
 
 const app = express();
 const server = http.createServer(app);
-const PORT = process.env.PORT || 3001; // 🔥 Use Render's assigned port
+
+app.use(express.static("public"));  // Serve frontend files
 
 const io = new Server(server, {
     cors: {
-      origin: "*", // Allow frontend to connect
+      origin: "https://southdevchat.onrender.com", // Use your deployed frontend URL
       methods: ["GET", "POST"]
     }
 });
 
-io.on('connection', (socket) => {
-    console.log('New user connected:', socket.id);
-    let user = '';
+io.on("connection", (socket) => {
+    console.log("New user connected:", socket.id);
+    
+    let user = "";
 
-    socket.on('newUser', (newUser) => {
+    socket.on("newUser", (newUser) => {
         user = newUser;
-        io.emit('newUser', newUser);
+        io.emit("newUser", newUser);
     });
 
-    socket.on('joinRoom', ({ user, room }) => {
+    socket.on("joinRoom", ({ user, room }) => {
         socket.join(room);
         socket.currentRoom = room;
-        io.to(room).emit('chatMessage', { user: 'System', msg: `${user} joined ${room}` });
+        io.to(room).emit("chatMessage", { user: "System", msg: `${user} joined ${room}` });
     });
 
-    socket.on('leaveRoom', ({ user, room }) => {
+    socket.on("leaveRoom", ({ user, room }) => {
         socket.leave(room);
-        io.to(room).emit('chatMessage', { user: 'System', msg: `${user} left ${room}` });
+        io.to(room).emit("chatMessage", { user: "System", msg: `${user} left ${room}` });
     });
 
-    socket.on('chatMessage', ({ user, msg, room }) => {
+    socket.on("chatMessage", ({ user, msg, room }) => {
         if (room) {
-            io.to(room).emit('chatMessage', { user, msg, room });
+            io.to(room).emit("chatMessage", { user, msg, room });
         } else {
-            io.emit('chatMessage', { user, msg });
+            io.emit("chatMessage", { user, msg });
         }
     });
 
-    socket.on('disconnect', () => {
+    socket.on("disconnect", () => {
         console.log(`${user} has left the chat`);
     });
 });
 
+// Listen on the correct port
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
